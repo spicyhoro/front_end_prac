82c0035c1d88ec916cfbade0957a8152f169f401
spicyhoro <spicyhoro777@gmail.com>
2019-12-28 19:48:48 +0900

    Comment 모델에 photo 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -22,6 +22,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=False)
     message = models.TextField()
+    photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
